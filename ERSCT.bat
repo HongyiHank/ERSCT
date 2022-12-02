3517b7107e2f29af8de649aa68d5f96c5f32deb7
HongyiHank <99805288+HongyiHank@users.noreply.github.com>
2022-12-02 23:26:41 +0800

    Update ERSCT.bat

--- ERSCT.bat
+++ ERSCT.bat
@@ -1,50 +1,17 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFAxRQgeKAE+1EbsQ5+n//NaikH4+YK8aSrz47oirEtMw3gjBR6UO4kZTm8RCBRhXHg==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJhZko0
-::ZQ05rAF9IBncCkqN+0xwdVsFAlXi
-::ZQ05rAF9IAHYFVzEqQIVGih3eGQ=
-::eg0/rx1wNQPfEVWB+kM9LVsJDCGwD0iOZg==
-::fBEirQZwNQPfEVWB+kM9LVsJDCGwD0iOZg==
-::cRolqwZ3JBvQF1fEqQK4wcXTpNoFxbA81UCbGjh+IhIBaLmViQu5l3ReDXELxTdz
-::dhA7uBVwLU+EWH2L4UA8LTM0
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATE3E0+LwJdZAWMNws=
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFAxRQgeKAE+1EbsQ5+n//NaikH4+YK8aSrz47oirEtMw3gjBR6UO4kZ/qvgvOFVdZhfL
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @Echo off
 Title ERSCT                                              
 Color 6   
 cd %USERPROFILE%/AppData/Roaming
 :caozuo  
-echo שששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששש
-echo ¦ד÷¸µn×kְפ¦sְֲֹא´«₪u¨ד¡G      
-echo    1.²M°£·ם«e¦sְֹ  
-echo    2.ֲא´«¦sְֹ     
-echo    3.¬d¬Ý¾Þ§@»¡©ת
-echo    4.¥´¶}¦sְֹ¦ל¸m   
-echo    5.ֲק¶}
-echo שששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששששש
-set /p n=½׀¿י₪J¿ן¶µ¡G 
+echo ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•
+echo ט‰¾חˆ¾ח™»ז³•ח’°ו­˜ז×”ט½‰ז›ו·¥ו…·ן¼      
+echo    1.ז¸…י™₪ח•¶ו‰ו­˜ז×”  
+echo    2.ט½‰ז›ו­˜ז×”     
+echo    3.ז¥ח‹ז“ה½ט××ז˜
+echo    4.ז‰“י–‹ו­˜ז×”ה½ח½®   
+echo    5.י›¢י–‹
+echo ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•ג•
+set /p n=ט«‹ט¼¸ו…¥י¸י …ן¼ 
 if "%n%"=="" cls&goto :caozuo 
 if "%n%"=="1" call :1 
 if "%n%"=="2" call :2 
@@ -56,29 +23,29 @@ pause
 goto :eof 
 
 :1 
-echo ¦¨¥\²M°£¦sְֹ 
+echo זˆוז¸…י™₪ו­˜ז×” 
 ren EldenRing ERSCT_Saves_1(Don't_move)
 goto :caozuo 
 
 :2 
-echo ֲא´«¦sְֹ₪₪½׀µ¥«Ý...
+echo ט½‰ז›ו­˜ז×”ה¸­ט«‹ח­‰ו¾…...
 ren EldenRing ERSCT_Saves_2(Don't_move)
 ren ERSCT_Saves_1(Don't_move) EldenRing
 ren ERSCT_Saves_2(Don't_move) ERSCT_Saves_1(Don't_move)
-echo ֲא´«§¹¦¨
+echo ט½‰ז›ו®זˆ
 goto :caozuo 
 
 :3 
-echo ¬d¬Ý¾Þ§@»¡©ת 
+echo ז¥ח‹ז“ה½ט××ז˜ 
 start https://hongyihanksblog.ml/ERSCT
 goto :caozuo 
 
 :4 
-echo ¥´¶}¦sְֹ¦ל¸m 
+echo ז‰“י–‹ו­˜ז×”ה½ח½® 
 start %USERPROFILE%/AppData/Roaming/EldenRing
 goto :caozuo 
 
 :5
-echo ֲק¶}
+echo י›¢י–‹
 exit 
 goto :caozuo
